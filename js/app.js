ddb0a077cae941ee36dcd761ffe2fc86abcd0584
Nabil CHIHEB <chiheb.design@gmail.com>
2024-10-08 18:23:12 +0000

    Update to vue3

--- js/app.js
+++ js/app.js
@@ -1,46 +1,42 @@
-var app = new Vue({
-  el: '#app',
-  data: {
-  	title: "",
-  	description : "",
-  	qty : 1,
-  	price : 10,
-    invoice: {
-    	number : 123,
-    	paid_at: null,
-    	currency : "DZD",
-    	paid : false,
-    	data   : new Date(),
-    	taxRate   : 0.19 ,
-    	from : 
-         { name : "Albert C. Morin", address : "3241 Coventry Court" , details : "Baton Rouge, LA 70814"}
-    	 ,
-    	to : 
-         { name : "Martin A. Selby", address : "450 Cinnamon Lane" , details : "San Antonio, TX 78202"}
-    	,
-    	products : [
-    		 { title : "test", description : "lorem" ,qty : 2, price : 20}
-    	]
-    }
-  },
-  methods :{
-  	add : function () {
-  		// body...
-       this.invoice.products.push({
-         title : this.title, description : this.description ,qty : this.qty, price : this.price
-       });
-  	},
-  	getTotal : function () {
-  		// body...
-  		console.log('getTotal')
-  	},
-  	getSubTotal : function () {
-  		// body...
-  	    var SubTotal = 0;
-  		for (var i = this.invoice.products.length - 1; i >= 0; i--) {
-  			SubTotal += this.invoice.products[i].price * this.invoice.products[i].qty
-  		}
-  		return SubTotal
-  	},
-  }
-})
+const { createApp, ref, computed } = Vue;
+
+createApp({
+	setup() {
+		const invoice = ref({
+			number: '001',
+			from: { name: 'Your Company', address: '123 Business St', details: 'City, Country' },
+			to: { name: 'Client Name', address: '456 Client Ave', details: 'Client City, Country' },
+			products: [
+				{ title: 'Service 1', description: 'Description 1', qty: 1, price: 100 }
+			],
+			taxRate: 0.1
+		});
+
+		const newProduct = ref({ title: '', description: '', qty: 0, price: 0 });
+
+		const getSubTotal = computed(() => {
+			return invoice.value.products.reduce((total, product) => {
+				return total + (product.qty * product.price);
+			}, 0);
+		});
+
+		function addProduct() {
+			if (newProduct.value.title && newProduct.value.qty > 0 && newProduct.value.price > 0) {
+				invoice.value.products.push({ ...newProduct.value });
+				newProduct.value = { title: '', description: '', qty: 0, price: 0 };
+			}
+		}
+
+		function removeProduct(index) {
+			invoice.value.products.splice(index, 1);
+		}
+
+		return {
+			invoice,
+			newProduct,
+			getSubTotal,
+			addProduct,
+			removeProduct
+		};
+	}
+}).mount('#app');
